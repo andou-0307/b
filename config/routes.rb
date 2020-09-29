26e92e18c4721284db1736222ac4e7b6fe71f0c9
andou-0307 <703515412307a@gmail.com>
2020-09-29 14:18:35 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tweets#index'
+  namespace :tweets do
+    resources :searches, only: :index
+  end
   resources :tweets do
     resources :comments, only: :create
-    collection do
-      get 'search'
-    end
   end
   resources :users, only: :show
   
